49394303aad96badb63075afc4bf5b1292c55966
Petrach95 <danieleptraccini@gmail.com>
2023-06-07 10:54:39 +0200

    modifica main

--- src/it/betacom/main/Main.java
+++ src/it/betacom/main/Main.java
@@ -2,4 +2,9 @@ package it.betacom.main;
 
 public class Main {
 
+	public static void main(String[] args) {
+		System.out.println("Avvio del programma");
+		System.out.println("Esecuzione del programma");
+		System.out.println("Fine del programma");
+	}
 }
