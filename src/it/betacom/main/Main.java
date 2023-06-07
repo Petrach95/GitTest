2f461026f5bfd6f67b74b93a8eb4e79b4fe3de1b
Petrach95 <danieleptraccini@gmail.com>
2023-06-07 11:42:31 +0200

    modifica3

--- src/it/betacom/main/Main.java
+++ src/it/betacom/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Avvio del programma");
 		System.out.println("Esecuzione del programma");
+		System.out.println("Fase 2 del programma");
 		System.out.println("Fine del programma");
 	}
 }
